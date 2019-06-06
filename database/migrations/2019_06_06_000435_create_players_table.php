cc2de5540e5518bcadc4d979efc1c5f8d872be03
Doroteo <dmacan@unipu.hr>
2019-06-06 02:08:59 +0200

    player model and table

---
+++ database/migrations/2019_06_06_000435_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('play_id');
+            $table->integer('team_id')->nullable();
+            $table->string('name')->nullable();
+            $table->boolean('won')->nullable();
+            $table->integer('points')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
